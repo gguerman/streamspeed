c294f53603b23df90a55b81f9633f32339f1ece1
gguerman <gguerman035@gmail.com>
2026-02-05 02:47:43 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+
+int main()
+{
+    char tecla1,tecla2;
+    int cantidad,contador;
+    double total;
+    double beats,min,bpm;
+    char tap;
+    clock_t ticksini, ticksend;
+    contador=0;
+    printf("elejir 2 teclas:");
+    tecla1=getch();
+    tecla2=getch();
+    printf("\n\ncantidad de pulsaciones:");
+    scanf("%i",&cantidad);
+
+    do{
+    tap=getch();
+    }while((tap!=tecla1)&&(tap!=tecla2));
+
+    ticksini=clock();
+
+    while(contador<cantidad-1)
+    {
+        tap=getch();
+        if((tap==tecla1)||(tap==tecla2))
+        {
+            contador=contador+1;
+        }
+    }
+
+    ticksend=clock();
+    total=(double)(ticksend-ticksini)/CLOCKS_PER_SEC;
+
+    beats=cantidad/4.0;
+    min=total/60;
+    bpm=beats/min;
+
+    printf("tiempo total:%.2f",total);
+    printf("\nvelocidad de tapping:%.1f bpm",bpm);
+    return 0;
+}
